cd992be0eb4cc889b2b8a72ea800780fa4681434
Travis Mottershead <travis.mottershead@verygoodsecurity.com>
2022-08-11 18:29:11 -0600

    Updated pagadito integration to be able to tokenize transactions.

--- lib/active_merchant/billing/gateways/pagadito.rb
+++ lib/active_merchant/billing/gateways/pagadito.rb
@@ -1,90 +1,135 @@
 module ActiveMerchant #:nodoc:
   module Billing #:nodoc:
     class PagaditoGateway < Gateway
-      self.test_url = 'https://example.com/test'
-      self.live_url = 'https://example.com/live'
-
-      self.supported_countries = ['US']
+      self.test_url = 'https://sandbox-api.pagadito.com/v1/'
+      self.live_url = 'https://api.pagadito.com/v1/'
       self.default_currency = 'USD'
-      self.supported_cardtypes = %i[visa master american_express discover]
 
-      self.homepage_url = 'http://www.example.net/'
-      self.display_name = 'New Gateway'
-
-      STANDARD_ERROR_CODE_MAPPING = {}
+      self.homepage_url = 'https://www.pagadito.com/'
+      self.display_name = 'Pagadito'
 
       def initialize(options = {})
-        requires!(options, :some_credential, :another_credential)
+        requires!(options, :username, :wsk)
+        @username, @wsk = options.values_at(:username, :wsk)
         super
       end
 
       def purchase(money, payment, options = {})
-        post = {}
-        add_invoice(post, money, options)
-        add_payment(post, payment)
-        add_address(post, payment, options)
-        add_customer_data(post, options)
+        # MultiResponse.run() do |r|
+        #   r.process {  }
+        # end
+        # post = {}
+        
+        # add_card(post, payment, options)
+        
+        # commit('payment', post)
 
-        commit('sale', post)
-      end
-
-      def authorize(money, payment, options = {})
-        post = {}
-        add_invoice(post, money, options)
-        add_payment(post, payment)
-        add_address(post, payment, options)
-        add_customer_data(post, options)
-
-        commit('authonly', post)
-      end
-
-      def capture(money, authorization, options = {})
-        commit('capture', post)
-      end
-
-      def refund(money, authorization, options = {})
-        commit('refund', post)
-      end
-
-      def void(authorization, options = {})
-        commit('void', post)
-      end
-
-      def verify(credit_card, options = {})
-        MultiResponse.run(:use_first_response) do |r|
-          r.process { authorize(100, credit_card, options) }
-          r.process(:ignore_result) { void(r.authorization, options) }
-        end
-      end
-
-      def supports_scrubbing?
-        true
-      end
-
-      def scrub(transcript)
-        transcript
+        tokenize(money, payment, options)
+        charge(money, payment, options)
       end
 
       private
 
-      def add_customer_data(post, options); end
-
-      def add_address(post, creditcard, options); end
-
-      def add_invoice(post, money, options)
-        post[:amount] = amount(money)
-        post[:currency] = (options[:currency] || currency(money))
+      def tokenize(amount, creditcard, options)
+        action = 'customer'
+        
+        post = {}
+        add_card(post, options, creditcard)
+        add_transaction(post, options, amount)
+        add_browser_info(post, options)
+        
+        commit(action, post)
       end
 
-      def add_payment(post, payment); end
+      def charge(amount, authorization, options)
+        action = 'charge'
 
-      def parse(body)
-        {}
+        post = {}
+        add_reference(post, authorization, options)
+        add_invoice(action, post, amount, options)
+        add_contact_details(post, options[:contact_details]) if options[:contact_details]
+        add_full_response(post, options)
+        add_metadata(post, options)
+
+        commit(action, post)
+      end
+
+      def add_card(post, options, creditcard)
+        card = {}
+
+        card[:number] = creditcard.number
+        card[:expirationDate] = "#{creditcard.month.to_s.rjust(2, '0')}/#{creditcard.year}"
+        card[:cvv] = creditcard.verification_value
+        card[:email] = options[:email] if options[:email]
+        add_card_name(card, creditcard)
+        add_billing_address(card, options)
+        
+        post[:card] = card
+      end
+
+      def add_card_name(card, creditcard)
+        if !creditcard.name.nil?
+          card[:cardHolderName] = creditcard.name
+          card[:name] = creditcard.name
+          name_split = creditcard.name.split(' ', 2)
+          card[:firstName] = name_split[0]
+          card[:lastName] = name_split[1]
+        else
+          card[:first_name] = creditcard.first_name if creditcard.first_name
+          card[:last_name] = creditcard.last_name if creditcard.last_name
+          card[:name] = creditcard.first_name + " " + creditcard.last_name
+        end
+      end
+      
+      def add_billing_address(parent, options)
+        billing_address = {}
+
+        if (address = options[:billing_address])
+          billing_address[:city] = address[:city] if address[:city]
+          billing_address[:state] = address[:state] if address[:state]
+          billing_address[:zip] = address[:zip] if address[:zip]
+          billing_address[:countryId] = country_code(address[:country]) if address[:country]
+          billing_address[:line1] = address[:address1]
+          billing_address[:phone] = address[:phone]
+        end
+
+        parent[:billingAddress] = billing_address
+      end
+
+      def add_transaction(post, options, amount)
+        transaction = {}
+        transaction[:merchantTransactionId] = options[:order_id].to_s if options[:order_id]
+        transaction[:currencyId] = (options[:currency] || currency(amount))
+        transaction[:transactionDetails] = [{
+          :quantity => 1,
+          :description => (options[:description] if options[:description]),
+          :amount => amount,
+        }]
+        post[:transaction] = transaction
+      end
+      
+      def country_code(country)
+        if country
+          country = ActiveMerchant::Country.find(country)
+          country.code(:numeric).value
+        end
+      rescue InvalidCountryCodeError
+      end
+
+      def add_browser_info(post, options)
+        browser_info = {}
+        browser_info[:customerIp] = options[:ip] if options[:ip]
+        browser_info[:deviceFingerprintID] = options[:device_id] if options[:device_id]
+        post[:browserInfo] = browser_info
       end
 
       def commit(action, parameters)
-        url = (test? ? test_url : live_url)
-        response = parse(ssl_post(url, post_data(action, parameters)))
+        url = (test? ? test_url : live_url) + action
+
+        headers = build_headers
+        puts "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+        puts headers
+        response = parse(ssl_post(url, post_data(parameters), headers))
 
         Response.new(
           success_from(response),
@@ -98,13 +143,29 @@ module ActiveMerchant #:nodoc:
         )
       end
 
+      def parse(body)
+        body.blank? ? {} :  JSON.parse(body)
+      end
+
       def success_from(response); end
 
       def message_from(response); end
 
       def authorization_from(response); end
 
-      def post_data(action, parameters = {}); end
+      def build_headers
+        auth_str = @username + ":" + @wsk
+        creds_b64 = Base64.strict_encode64(auth_str)
+        return {
+          'Authorization'	=> "Basic #{creds_b64}",
+          # 'Authorization'	=> "Basic #{auth_str}",
+          'Content-Type'	=> 'application/json'
+        }
+      end
+
+      def post_data(parameters = {})
+        JSON.generate(parameters)
+      end
 
       def error_code_from(response)
         unless success_from(response)
